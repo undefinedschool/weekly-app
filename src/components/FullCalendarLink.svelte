5db2795509c2011dcfacc11a26f175ae999ea23f
Nicolás Quiroz <nh.quiroz@gmail.com>
2019-12-12 02:58:46 -0300

    mvp v0

--- src/components/FullCalendarLink.svelte
+++ src/components/FullCalendarLink.svelte
@@ -1,5 +1,8 @@
 <p class="font-light text-sm text-right -mt-1 -mr-1 mb-4 text-gray-us link">
-  <a href="https://trello.com/b/mUf0huXz/undefined-school">
+  <a
+    href="https://trello.com/b/mUf0huXz/undefined-school"
+    target="_blank"
+    rel="noopener">
     Ver calendario completo
   </a>
 </p>
